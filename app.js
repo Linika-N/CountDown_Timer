5511efafdf5795007e8ffde2ba33e8a6bb5365d8
Linika <lidya-novikova.95@yandex.ru>
2022-07-25 13:26:10 +0300

    first version is ready

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 let today=new Date()
 let new_year=new Date(2023,0,1,00)
-let result
 let _second = 1000;
 let _minute = _second * 60;
 let _hour = _minute * 60;
 let _day = _hour * 24;
 
-let day,hours,minutes,seconds
+let days,hours,minutes,seconds,result
 
 
 result=new_year-today
 
 
-day=Math.floor(result/_day)
+days=Math.floor(result/_day)
+hours=Math.floor(result%_day/_hour)
+minutes=Math.floor(result%_day%_hour/_minute)
+seconds=Math.floor(result%_day%_hour%_minute/_second)
 
 
-console.log(days)
-console.log(today)
-console.log(new_year)
-console.log(day)
+document.getElementById("days").innerHTML= `${days} <br> days`
+document.getElementById("hours").innerHTML= `${hours} <br> hours`
+document.getElementById("minutes").innerHTML= `${minutes} <br> minutes`
+document.getElementById("seconds").innerHTML= `${seconds} <br> seconds`
 
-document.getElementById("days").innerHTML= `${day} <br> days`
+
 
 
