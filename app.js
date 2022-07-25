76b99576c7fa606ac10b0fc2855b9438880d688f
Linika <lidya-novikova.95@yandex.ru>
2022-07-25 14:04:44 +0300

    upgrade js

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
-let today=new Date()
-let new_year=new Date(2023,0,1,00)
+let today=new Date();
+let new_year=new Date(2023,0,1,00)-today;
 let _second = 1000;
 let _minute = _second * 60;
 let _hour = _minute * 60;
-let _day = _hour * 24;
-
-let days,hours,minutes,seconds,result
+let _day =_hour * 24;
 
 
-result=new_year-today
+let days,hours,minutes,seconds
 
-
-days=Math.floor(result/_day)
-hours=Math.floor(result%_day/_hour)
-minutes=Math.floor(result%_day%_hour/_minute)
-seconds=Math.floor(result%_day%_hour%_minute/_second)
+days=Math.floor(new_year/_day)
+hours=Math.floor(new_year%_day/_hour)
+minutes=Math.floor(new_year%_day%_hour/_minute)
+seconds=Math.floor(new_year%_day%_hour%_minute/_second)
 
 
 document.getElementById("days").innerHTML= `${days} <br> days`
